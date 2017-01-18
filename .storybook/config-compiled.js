ba9cddbe7f76876bf2935f2441c4edc1b5c2c0d8
Johnny Gu <johnny@wiredcraft.com>
2017-01-18 10:29:39 +0800

    init

---
+++ .storybook/config-compiled.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var _storybook = require('@kadira/storybook');
+
+function loadStories() {
+  require('../src/stories');
+}
+
+(0, _storybook.configure)(loadStories, module);
+
+//# sourceMappingURL=config-compiled.js.map
