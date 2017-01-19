fa8e74e23093b9815999e1e11ab09c0f7f8351b8
Johnny Gu <johnny@wiredcraft.com>
2017-01-20 00:40:02 +0800

    logic

--- src/stories/App.js
+++ src/stories/App.js
@@ -17,7 +17,6 @@ export default class App extends Component {
             off: true,
             strict: false,
             count: 0,
-
         };
         // init common use audio
         const audio1 = new Audio(simon1);
@@ -29,7 +28,7 @@ export default class App extends Component {
             2: audio2,
             3: audio3,
             4: audio4,
-            song: '',
+            song: [],
         };
         this.colorClass = {
             1: "hightGreen",
@@ -38,8 +37,16 @@ export default class App extends Component {
             4: "hightYellow",
         };
         this.btnRef = {
-
         };
+        this.control = {
+            userInputCount: 0,
+            autoPlaying: false,
+            userNotInteract: true,
+        }
+    }
+
+    componentWillUnmount() {
+        this.reset()
     }
 
     onOffChange = () => {
@@ -47,14 +54,22 @@ export default class App extends Component {
         // reset every thing
         if (!off) {
             this.reset()
-        }else {
-            this.setState({off: !off});
         }
+        this.setState({off: !off});
     };
 
     reset = () => {
-        this.setState({off: true, count: 0, strict: false});
-    }
+        this.control = {
+            userInputCount: 0,
+            autoPlaying: false
+        };
+        this.setState({count: 0, strict: false});
+        var id = window.setTimeout(function() {}, 0);
+
+        while (id--) {
+            window.clearTimeout(id); // will do nothing if no timeout with id is present
+        }
+    };
 
     onStrictChange = () => {
         const strict = this.state.strict;
@@ -63,21 +78,28 @@ export default class App extends Component {
 
     onStart = () => {
         // generate random song
-        var song = '';
+        var song = [];
         while(song.length < 20) {
-            song += this.getRandomInt(1,4).toString();
+            song.push(this.getRandomInt(1,4).toString());
         }
         this.contextProps.song = song;
+        this.control.userInputCount = 0;
+        this.setState({count:0});
+        var id = window.setTimeout(function() {}, 0);
 
+        while (id--) {
+            window.clearTimeout(id); // will do nothing if no timeout with id is present
+        }
         // start autoplay
-        this.autoPlay(4)
+        this.autoPlay(1)
     }
 
     autoPlay = (count) => {
-
+        console.log(count)
+        if (this.state.off) { return }
+        this.control.autoPlaying = true;
         const that = this;
-        const list = this.contextProps.song.slice(count).split('');
-
+        const list = this.contextProps.song;
         let num = 0;
         const play = () => {
             const key = list[num];
@@ -89,8 +111,20 @@ export default class App extends Component {
             }, 500);
             setTimeout(() => {
                 // ...
-                if (num++ < count) {
+                if (++num < count) {
                     play();
+                }else {
+                    that.control.autoPlaying = false;
+                    that.control.userInputCount = 0;
+                    that.control.userNotInteract = true;
+                    setTimeout(()=>{
+                        if (that.control.userNotInteract) {
+                            // timeout case
+                            setTimeout(()=>{
+                                that.autoPlay(count);
+                            }, 1000)
+                        }
+                    }, 3000)
                 }
             }, 1000);
         };
@@ -107,20 +141,52 @@ export default class App extends Component {
     }
 
     onMouseDown = (e)=> {
-        if (this.state.off) { return }
+        if (this.state.off || this.state.autoPlaying) { return }
+        if (this.state.userInputCount > this.state.count) {
+            return
+        }
         const key = e.target.id;
         console.log(key);
         this.contextProps[key].play();
-
+        this.control.userNotInteract = false;
         e.target.className += " " + this.colorClass[e.target.id];
-        console.log(e.target.className)
+
     };
 
     onMouseUp = (e)=> {
-        if (this.state.off) { return }
-        console.log(e.target.className);
+        const {off,autoPlaying,userInputCount,count} = this.state;
+        if (off || autoPlaying) { return }
+        if (userInputCount > count) { return }
         const newClass = e.target.className.replace(this.colorClass[e.target.id], '');
         e.target.className = newClass;
+        // do the check
+        debugger
+        console.log(e.target.id, this.contextProps.song[userInputCount])
+        if (e.target.id != this.contextProps.song[userInputCount]) {
+            //error
+            if (this.state.strict) {
+                this.reset()
+                this.autoPlay(1)
+            }else {
+                const that = this;
+                this.setState({userInputCount: 0});
+                setTimeout(()=> {
+                    that.autoPlay(count + 1);
+                }, 1000)
+            }
+        }else {
+
+            if (userInputCount === count) {
+                this.setState({count: count + 1, userInputCount: 0});
+                const that = this
+                setTimeout(()=>{
+                    that.autoPlay(count + 2)
+                }, 1000)
+            }else {
+                this.setState({userInputCount: userInputCount + 1});
+            }
+        }
+
     }
 
     render() {
